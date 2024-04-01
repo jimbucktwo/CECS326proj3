b88dfb434b299e4efdb07cf4ae22b9a1bdf4db3e
jimbucktwo <92623786+jimbucktwo@users.noreply.github.com>
2024-04-01 14:53:42 -0700

    finished with comments

--- program.cpp
+++ program.cpp
@@ -15,6 +15,7 @@ int processes[num_customers] = {0, 1, 2, 3, 4};
 string commands[num_resources]; //used to take care of the user inputs
 
 
+// safety algorithm
 bool safety_algorithm(int customer_num) {
     for (int i = 0; i < num_customers; i++){
         for(int j = 0; j < num_resources; j++) {
@@ -69,7 +70,7 @@ bool safety_algorithm(int customer_num) {
     return true;
 }
 
-
+// satisfies request if it is in a safe state
 int request_resource(int customer_num, int request[]) {
     if (safety_algorithm(customer_num)) {
         for(int i = 0; i < num_resources; i++) {
@@ -86,12 +87,14 @@ int request_resource(int customer_num, int request[]) {
     return -1;
 }
 
+//releases resources specified
 int release_resources(int customer_num, int release[]) {
     for(int i = 0; i < num_resources; i++) {
         available[i] += release[i];
     }
 }
 
+//helper function to print 2d arrays
 void print_array(int array[][num_resources]) {
     for(int i = 0; i < num_customers; i++) {
         cout << "[ ";
@@ -102,17 +105,6 @@ void print_array(int array[][num_resources]) {
     }
 }
 
-void tokenize(string s, string del = " ") {
-    int i = 0;
-    int start, end = -1 * del.size();
-    do {
-        start = end + del.size();
-        end = s.find(del,start);
-        commands[i] = s.substr(start,end - start);
-        i++;
-    } while (end != -1);
-}
-
 
 int main(int argc, char *argv[]) {
     bool valid = true;
@@ -121,6 +113,7 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
+    // initiates available array to values inputted
     for (int i = 0; i < num_resources; i++) {
         int intValue = stoi(argv[i + 1]);
         available[i] = intValue;
@@ -129,18 +122,21 @@ int main(int argc, char *argv[]) {
     ifstream readFile("input.txt");
     if (!readFile.is_open()) return -1;
 
+    // initiates maximum array to values in the text file
     for (int i = 0; i < num_customers; i++){
         for(int j = 0; j < num_resources; j++) {
             readFile >> maximum[i][j];
         }
     }
 
+    // initiates need array
     for (int i = 0; i < num_customers; i++){
         for(int j = 0; j < num_resources; j++) {
             need[i][j] = maximum[i][j] - allocation[i][j];
         }
     }
 
+    // main logic. Continues until the user terminates the program, takes in user input to RQ or RL
     while (valid) {
     cout << endl << "Enter your command (RQ, RL, *) followed by customer number and resources: ";
     for (int i = 0; i < num_resources + 2; i++) {
@@ -155,6 +151,7 @@ int main(int argc, char *argv[]) {
         for(int i = 2; i < num_resources + 2; i++) {
             requests[i - 2] = stoi(commands[i]);
         }
+        // runs the safety algorithm to determine whether to complete the request
         request_resource(stoi(commands[1]), requests);
     } else if (commands[0] == "RL") {
         for(int i = 0; i < num_resources; i++) {
@@ -177,7 +174,6 @@ int main(int argc, char *argv[]) {
     } else {
         cout << "Invalid. Enter one of the three commands";
     }
-
     }
 
 }
